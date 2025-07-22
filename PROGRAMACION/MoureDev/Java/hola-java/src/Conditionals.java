3fba22f759b04bf00e3699709b7cf85b9fdabe5f
JDevLogic <israelruales7@gmail.com>
2025-07-22 15:42:39 +0200

    Añadidos ejemplos de condicionales en Java

---
+++ PROGRAMACION/MoureDev/Java/hola-java/src/Conditionals.java
@@ -0,0 +1,26 @@
+public class Conditionals {
+
+    public static void main(String[] args) {
+
+    // Condicionales
+
+    var age = 18;
+
+    System.out.println(age >= 18);
+
+    if (age > 18) {
+        System.out.println("El usuario es mayor de edad.");
+    }
+
+    else if(age == 18){
+            System.out.println("El usuario acaba de cumplir 18.");
+
+    }
+
+    else {
+        System.out.println("El usuario es menor de edad.");
+
+    }
+
+    }
+}
