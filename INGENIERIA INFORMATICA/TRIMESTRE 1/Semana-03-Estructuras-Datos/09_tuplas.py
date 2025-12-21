3e14c4c83523809af0d294c84b39375dbc34ef54
JDevLogic <israelruales7@gmail.com>
2025-12-21 13:57:15 +0100

    Añadidos tema de tuplas (fundamentos y uso básico)

---
+++ INGENIERIA INFORMATICA/TRIMESTRE 1/Semana-03-Estructuras-Datos/09_tuplas.py
@@ -0,0 +1,43 @@
+#=============================
+# TUPLAS(Fundamentos)
+# ============================
+
+# Una tupla es una estructura de datos similar a una lista,
+# pero es INMUTABLE (no se puede modificar).
+
+numeros = (1,2,3,4)
+nombres = ("Luis", "Miguel","Bryan")
+
+print(numeros)
+print(nombres)
+
+
+# Esto NO se puede hacer en una tupla porque es inmutable
+# numeros[0] = 10
+
+# Acceso por índice (igual que en listas)
+
+print(numeros[0])
+print(nombres[1])
+
+
+# Longitud de una tupla
+
+print(len(numeros))
+print(len(nombres))
+
+
+# Recorrer una tupla con for
+
+for numero in numeros:
+    print(numero)
+
+for nombre in nombres:
+    print(nombre)
+
+
+# Las tuplas se usan mucho para:
+# - Retornos múltiples de funciones
+# - Desempaquetado de valores
+# - Datos que no deben cambiar (coordenadas, configuraciones, resultados)
+
