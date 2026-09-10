a9ed5f3a20d3bc5c6e7f243520fb707884375fdc
JDevLogic <israelruales7@gmail.com>
2026-09-11 01:19:23 +0200

    docs: añadir fundamentos de diccionarios

---
+++ INGENIERIA INFORMATICA/CURSO-1/SEMESTRE-1/Programacion-I/Semana-03-Estructuras-Datos/10_diccionarios.py
@@ -0,0 +1,18 @@
+# ==========================
+# DICCIONARIOS (Fundamentos)
+# ==========================
+
+# ¿Qué es un diccionario?
+
+# Un diccionario es una estructura de datos que almacena
+# información mediante pares clave-valor.
+
+# A diferencia de una lista, donde accedemos mediante
+# posiciones (índices), en un diccionario accedemos
+# mediante claves.
+
+# Lista:
+# posición -> valor
+
+# Diccionario:
+# clave -> valor
